1081403d755f98d06932d1bd13ba45773c346744
I-A11 <d.islam1983@gmail.com>
2021-06-13 20:33:18 +1000

    adding cart page and style it

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const toggle = document.querySelector(".fa-bars");
 const menuItems = document.getElementById("menuItems");
+let productImage = document.querySelector(".product-image");
+let smallImage = document.querySelectorAll(".small-img");
 
 menuItems.style.maxHeight = "0px";
 const menuToggle = () => {
@@ -11,3 +13,16 @@ const menuToggle = () => {
 };
 
 toggle.addEventListener("click", menuToggle);
+
+smallImage[0].onclick = function () {
+  productImage.src = smallImage[0].src;
+};
+smallImage[1].onclick = function () {
+  productImage.src = smallImage[1].src;
+};
+smallImage[2].onclick = function () {
+  productImage.src = smallImage[2].src;
+};
+smallImage[3].onclick = function () {
+  productImage.src = smallImage[3].src;
+};
