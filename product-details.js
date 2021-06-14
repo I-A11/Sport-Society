99c6a51315f2d0a1de4cf1d2bfbf1a985961d3a8
I-A11 <d.islam1983@gmail.com>
2021-06-14 12:32:07 +1000

    fix product detail bug

--- product-details.js
+++ product-details.js
@@ -1,6 +1,20 @@
 const productImage = document.querySelector(".product-image");
 const smallImage = document.querySelectorAll(".small-img");
 
+const toggle = document.querySelector(".fa-bars");
+const menuItems = document.getElementById("menuItems");
+
+menuItems.style.maxHeight = "0px";
+const menuToggle = () => {
+  if (menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "200px";
+  } else {
+    menuItems.style.maxHeight = "0px";
+  }
+};
+
+toggle.addEventListener("click", menuToggle);
+
 smallImage[0].onclick = function () {
   productImage.src = smallImage[0].src;
 };
